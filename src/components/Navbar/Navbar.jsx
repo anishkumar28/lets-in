a3ab8bf8f068d8038afdc815112ae48323753f93
Anish Kumar <anishkumar6527@gmail.com>
2024-01-27 20:51:46 +0530

    creating Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import Logo from "../../assets/logo_black.png"
+import { NavLink } from "react-router-dom";
+
+
+function Navbar(){
+    return(
+        <nav>
+        <div className=" flex flex-nowrap justify-between mt-2 m-8 p-4 border-b-2 border-black">
+        <div>
+            <button className="">
+                <img className= "h-12"src={Logo} alt=""/>
+            </button>
+        </div>
+        <div className="flex flex-nowrap shrink p-4 gap-1">
+            <button>
+            <NavLink
+                    to="/dashboard"
+                    className={() => `pl-2 pr-2 pt-2 pb-2 hover:bg-black hover:text-white hover:rounded `}>
+                        Dashboard
+                    </NavLink>
+                    
+            </button>
+            <button className="pl-2 pr-2 pt-2 pb-2 hover:bg-black hover:text-white hover:rounded ">
+            <NavLink
+                to="/about">
+                About
+                </NavLink>
+            </button>
+            <button className="pl-2 pr-2 pt-2 pb-2 hover:bg-black hover:text-white hover:rounded ">
+                <NavLink
+                to="/login">
+                Logout
+                </NavLink>
+            </button>
+        </div>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar;
