6dae73e7698748a1b616ca4c02d005956756d294
Anish Kumar <anishkumar6527@gmail.com>
2024-02-02 20:10:41 +0530

    Adding routes to Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
-import Dashboard from './components/Dashboard/Dashboard.jsx'
-import ModalPopup from './ModalPopup.jsx'
-import FormNavbar from './components/FormNavbar/FormNavbar.jsx'
-import ListContainer from './components/ListContainer/ListContainer.jsx'
 import Sidebar from './components/Sidebar/Sidebar.jsx'
-import Settings from './components/Settings/Settings.jsx'
+import BoardSection from './components/Pages/BoardSection.jsx'
+import ContactsSection from './components/Pages/ContactsSection.jsx'
+import SettingsSection from './components/Pages/SettingsSection.jsx'
+import AccountSection from './components/Pages/AccountsSection.jsx'
+
 
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='dashboard' element={<ListContainer />} >
-        <Route path='board' element={<Dashboard/>} />
-        <Route path='setting' element={<Settings />} />
-      </Route>
+      <Route path='dashboard' element={<Sidebar />} />
+        <Route path='/board' element={<BoardSection/>}/>
+        <Route path='contacts' element={<ContactsSection/>}/>
+        <Route path='settings' element={<SettingsSection />}/>
+        <Route path='account' element={<AccountSection/>}/>
   
     </Routes>
     </>
